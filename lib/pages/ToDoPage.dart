8095ed0aacef2f4b40430cba3d27268c81378aef
bereketsewnet <bereketsewnet@users.noreply.github.com>
2024-08-19 23:08:57 +0300

    init commit

--- lib/pages/ToDoPage.dart
+++ lib/pages/ToDoPage.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
-class ToDoPage extends StatelessWidget {
+class ToDoPage extends StatefulWidget {
   const ToDoPage({super.key});
 
   @override
+  State<ToDoPage> createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: TextField(
+          controller: controller,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ),
+    );
   }
 }
