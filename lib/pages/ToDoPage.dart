7f09d151f1cbc2ea9c1f6532fefa8001ffb72bca
bereketsewnet <bereketsewnet@users.noreply.github.com>
2024-08-19 22:58:34 +0300

    Initial commit

---
+++ lib/pages/ToDoPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ToDoPage extends StatelessWidget {
+  const ToDoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
